4abb37678156037bd7ba068b169731297e92c6ed
Mirte Draaijer <midraaijer@bioinf.nl>
2025-03-13 12:24:40 +0100

    Trying to fix table unsuccesfull

--- app/server.R
+++ app/server.R
@@ -2,119 +2,119 @@ source("functions.R")
 source("../config.R")
 
 server <- function(input, output, session) {
-    
-    # Read in .tsv files with expression data and metadata
-    expression_data <- read_feather(paste0(DATA_DIR, "expression_data_subset.tsv"))
-    meta_data <- read_feather(paste0(DATA_DIR, "meta_data.tsv"))
-    
-    # Show/hide filter panels in UI, depending on chosen 'use_case'
-    observeEvent(input$use_case, {
-        if (input$use_case == "explore_expression") {
-            show("genes_accordion")
-            show("cancer_types_accordion")
-        } else {
-            show("genes_accordion")
-            hide("cancer_types_accordion")
-            
-        }
-    })
-    
-    
-    # Updates all dropdown inputs using server-side selectize
-    updateSelectizeInput(session, 
-                         'gene_name', 
-                         choices = expression_data$gene, 
-                         selected = expression_data$gene[1], 
-                         server = TRUE)
-    
-    updateSelectizeInput(session, 
-                         'onco_type', 
-                         choices = sort(meta_data$OncotreePrimaryDisease), 
-                         selected = "Acute Myeloid Leukemia",
-                         server = TRUE)
-    
-    
-    updateSelectizeInput(session, 
-                         "sex", 
-                         choices = unique(meta_data$Sex), 
-                         selected = c("Female", "Male", "Unknown"))
-    
-    updateSelectizeInput(session, 
-                         "race", 
-                         choices = meta_data$PatientRace, 
-                         selected = c("caucasian", "asian", "black_or_african_american",
-                                      "african", "american_indian_or_native_american", 
-                                      "east_indian", "north_african", "hispanic_or_latino", "unknown"))
-    
-    updateSelectizeInput(session, 
-                         "age_category", 
-                         choices = meta_data$AgeCategory, 
-                         selected = c("Fetus", "Pediatric", "Adult", "Unknown"))
-    
-    # Calls function to filter metadata based on user input for cancer type and other metadata
-    filtered_metadata <- reactive({
-        filter_metadata(meta_data, input)
-    })
-    
-    # Calls function to merge filtered metadata with the corresponding expression data
-    merged_data <- reactive({
-        merge_data(filtered_metadata(), expression_data)
-    })
-    
-    # Calls function to filter the merged data on the user-chosen gene(s)
-    selected_data <- reactive({
-        filter_gene(merged_data(), input)
-    })
-    
-    # Generate output for XY plots
-    output$plot <- renderPlotly({
+  
+  # Read in .tsv files with expression data and metadata
+  expression_data <- read_feather(paste0(DATA_DIR, "expression_data_subset.tsv"))
+  meta_data <- read_feather(paste0(DATA_DIR, "meta_data.tsv"))
+  
+  # Show/hide filter panels in UI, depending on chosen 'use_case'
+  observeEvent(input$use_case, {
+    if (input$use_case == "explore_expression") {
+      show("genes_accordion")
+      show("cancer_types_accordion")
+    } else {
+      show("genes_accordion")
+      hide("cancer_types_accordion")
       
-      # Retrieve data from reactive function
-      data <- selected_data()
-      
-      # Prevent error where plot tries to render before data has loaded in
-      req(nrow(data) >= 1)
-      
-      plot <- xyplots(data, type = switch(input$summary_type, 
-                                          "Box Plot" = "boxplot",
-                                          "Violin Plot" = "violin",
-                                          "Bar Plot" = "bar"))
-      
-      if (input$summary_type == "Box Plot"){
-        output = ggplotly(plot)
-        output$x$data[[1]]$marker = list(opacity = 0)
-        output}
-      else{
-        plot
-      }
-    })
+    }
+  })
+  
+  
+  # Updates all dropdown inputs using server-side selectize
+  updateSelectizeInput(session, 
+                       'gene_name', 
+                       choices = expression_data$gene, 
+                       selected = expression_data$gene[1], 
+                       server = TRUE)
+  
+  updateSelectizeInput(session, 
+                       'onco_type', 
+                       choices = sort(meta_data$OncotreePrimaryDisease), 
+                       selected = "Acute Myeloid Leukemia",
+                       server = TRUE)
+  
+  
+  updateSelectizeInput(session, 
+                       "sex", 
+                       choices = unique(meta_data$Sex), 
+                       selected = c("Female", "Male", "Unknown"))
+  
+  updateSelectizeInput(session, 
+                       "race", 
+                       choices = meta_data$PatientRace, 
+                       selected = c("caucasian", "asian", "black_or_african_american",
+                                    "african", "american_indian_or_native_american", 
+                                    "east_indian", "north_african", "hispanic_or_latino", "unknown"))
+  
+  updateSelectizeInput(session, 
+                       "age_category", 
+                       choices = meta_data$AgeCategory, 
+                       selected = c("Fetus", "Pediatric", "Adult", "Unknown"))
+  
+  # Calls function to filter metadata based on user input for cancer type and other metadata
+  filtered_metadata <- reactive({
+    filter_metadata(meta_data, input)
+  })
+  
+  # Calls function to merge filtered metadata with the corresponding expression data
+  merged_data <- reactive({
+    merge_data(filtered_metadata(), expression_data)
+  })
+  
+  # Calls function to filter the merged data on the user-chosen gene(s)
+  selected_data <- reactive({
+    filter_gene(merged_data(), input)
+  })
+  
+  # Generate output for XY plots
+  output$plot <- renderPlotly({
     
-    output$heatmap <- renderPlotly({
-        
-        # Retrieve data from reactive function
-        data <- selected_data()
-        
-        # Prevent error where plot tries to render before data has loaded in
-        req(nrow(data) >= 1)
-        
-        heatmap <- generate_heatmap(data)
-        
-        
-    })
+    # Retrieve data from reactive function
+    data <- selected_data()
     
-    # Generate output for data table
-    output$data <- renderDataTable({
-      # Retrieve data from reactive function
-      data <- selected_data()
-      
-      # Prevent error where plot tries to render before data has loaded in
-      req(nrow(data) >= 1)
-      
-
-      generate_datatable(data, filter = "top")
-
-      
-    })
+    # Prevent error where plot tries to render before data has loaded in
+    req(nrow(data) >= 1)
+    
+    plot <- xyplots(data, type = switch(input$summary_type, 
+                                        "Box Plot" = "boxplot",
+                                        "Violin Plot" = "violin",
+                                        "Bar Plot" = "bar"))
+    
+    if (input$summary_type == "Box Plot"){
+      output = ggplotly(plot)
+      output$x$data[[1]]$marker = list(opacity = 0)
+      output}
+    else{
+      plot
+    }
+  })
+  
+  output$heatmap <- renderPlotly({
+    
+    # Retrieve data from reactive function
+    data <- selected_data()
+    
+    # Prevent error where plot tries to render before data has loaded in
+    req(nrow(data) >= 1)
+    
+    heatmap <- generate_heatmap(data)
     
     
+  })
+  
+  # Generate output for data table
+  output$data <- renderDataTable({
+    # Retrieve data from reactive function
+    data <- selected_data()
+    
+    # Prevent error where plot tries to render before data has loaded in
+    req(nrow(data) >= 1)
+    
+    
+    generate_datatable(data, filter = "top")
+    
+    
+  })
+  
+  
 }
