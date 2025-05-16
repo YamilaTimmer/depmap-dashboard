5ecd96a97b3afc1afca876ffb760d23dfd60a19d
MirteDraaijer <mirtedraaijer@outlook.com>
2025-05-16 11:22:17 +0200

    Added help page for compare pathways use-case

--- app/ui.R
+++ app/ui.R
@@ -441,7 +441,75 @@ ui <- page_fillable(
                                                        'research' column.")
                                                    )),
                                          
-                                         nav_panel("Help", value = "help_compare")
+                                         nav_panel("Help", 
+                                                   value = "help_compare",
+                                                   fluidPage(
+                                                     h3("Compare pathways"),
+                                                     p("This use case can be used 
+                                                       to compare humane biological 
+                                                       pathways, which are 
+                                                       collections of genes working 
+                                                       together in order to fulfill 
+                                                       a biological process.
+                                                       Expression is shown as 
+                                                       log2 TPM. TPM represents 
+                                                       the number of transcripts 
+                                                       from the gene, per one 
+                                                       million RNA molecules in 
+                                                       the RNA sample. The log2 
+                                                       transformation is then 
+                                                       applied to normalize 
+                                                       the data."),
+                                                     
+                                                     h3("Selection"),
+                                                     p("On the left side, select 
+                                                       the pathway you are 
+                                                       interested in. The dropdown 
+                                                       selection for 'select pathways' 
+                                                       contains all humane pathways 
+                                                       from the", 
+                                                       a("KEGG pathway database",
+                                                         href = "https://www.kegg.jp/kegg/kegg2.html",
+                                                         target = "_blank")
+                                                       ," 
+                                                       Selecting a pathway will 
+                                                       auto-select all genes 
+                                                       that belong to the chosen 
+                                                       pathway. Select one or 
+                                                       more types of cancer to 
+                                                       compare expression of genes 
+                                                       in the chosen pathway, 
+                                                       across cancer types."),
+                                                     
+                                                     h3("Heatmap"),
+                                                     p("The result can be viewed 
+                                                       in the heatmap, with genes 
+                                                       on the x-axis and cancer 
+                                                       types on the y-axis. 
+                                                       Different color palettes 
+                                                       can be chosen and the colors 
+                                                       resemble the expression 
+                                                       rates in log2 TPM"),
+                                                     
+                                                     h3("Data"),
+                                                     p("On the 'data' tab a data 
+                                                       table containing the data 
+                                                       is shown. At the top, you 
+                                                       can select which columns 
+                                                       to show. Furthermore, you 
+                                                       can download the raw data 
+                                                       as a .csv or .xlsx file 
+                                                       through the download button. 
+                                                       Additional information on 
+                                                       the gene(s) can be found 
+                                                       by clicking on the gene 
+                                                       name in the data table. 
+                                                       Additional information on 
+                                                       the gene + cancer type 
+                                                       can be found by clicking 
+                                                       on 'PubMed' in the 
+                                                       'research' column.")
+                                                   ))
                                          
                                          
                          )
